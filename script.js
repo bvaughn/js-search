00e537931702510a66a22af3e1076520c11dc183
bvaughn <brian.david.vaughn@gmail.com>
2015-08-01 09:04:50 -0700

    Updated demo to use new stop-words index

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 var search;
-var indexOnTitle = true, indexOnAuthor = true;
 var allBooks = [];
+
+var indexOnAuthorCheckbox = document.getElementById('indexOnAuthorCheckbox');
+var indexStrategySelect = document.getElementById('indexStrategySelect');
+var removeStopWordsCheckbox = document.getElementById('removeStopWordsCheckbox');
+var indexOnTitleCheckbox = document.getElementById('indexOnTitleCheckbox');
+
+var rebuildAndRerunSearch = function() {
+  rebuildSearchIndex();
+  searchBooks();
+};
+
+indexOnAuthorCheckbox.onchange = rebuildAndRerunSearch;
+indexStrategySelect.onchange = rebuildAndRerunSearch;
+removeStopWordsCheckbox.onchange = rebuildAndRerunSearch;
+indexOnTitleCheckbox.onchange = rebuildAndRerunSearch;
 
 var rebuildSearchIndex = function() {
   search = new JsSearch('isbn');
-  if (indexOnTitle) {
+  var indexStrategy = eval(`new ${indexStrategySelect.value}()`);
+  if (removeStopWordsCheckbox.checked) {
+    search.indexStrategy = new StopWordsIndexStrategyDecorator(new PrefixIndexStrategy());
+  } else {
+    search.indexStrategy = indexStrategy;
+  }
+  if (indexOnTitleCheckbox.checked) {
     search.addIndex('title');
   }
-  if (indexOnAuthor) {
+  if (indexOnAuthorCheckbox.checked) {
     search.addIndex('author');
   }
   search.addDocuments(allBooks);
@@ -34,13 +54,13 @@ var updateBooksTable = function(books) {
 
     var titleColumn = document.createElement('td');
     titleColumn.innerHTML =
-      indexOnTitle ?
+      indexOnTitleCheckbox.checked ?
         tokenHighlighter.highlight(book.title, tokens) :
         book.title;
 
     var authorColumn = document.createElement('td');
     authorColumn.innerHTML =
-      indexOnAuthor ?
+      indexOnAuthorCheckbox.checked ?
         tokenHighlighter.highlight(book.author, tokens) :
         book.author;
 
@@ -51,35 +71,25 @@ var updateBooksTable = function(books) {
 
     indexedBooksTBody.appendChild(tableRow);
   }
-
-  var searchBooks = function() {
-    var query = searchInput.value;
-    var results = search.search(query);
-
-    updateBookCount(results.length);
-
-    if (results.length > 0) {
-      updateBooksTable(results);
-    } else if (!!query) {
-      updateBooksTable([]);
-    } else {
-      updateBookCount(allBooks.length);
-      updateBooksTable(allBooks);
-    }
-  };
-
-  document.getElementById('authorCheckbox').onchange = function() {
-    indexOnAuthor = !indexOnAuthor;
-    rebuildSearchIndex();
-    searchBooks();
-  };
-  document.getElementById('titleCheckbox').onchange = function() {
-    indexOnTitle = !indexOnTitle;
-    rebuildSearchIndex();
-    searchBooks();
-  };
-  searchInput.oninput = searchBooks;
 };
+
+var searchBooks = function() {
+  var query = searchInput.value;
+  var results = search.search(query);
+
+  updateBookCount(results.length);
+
+  if (results.length > 0) {
+    updateBooksTable(results);
+  } else if (!!query) {
+    updateBooksTable([]);
+  } else {
+    updateBookCount(allBooks.length);
+    updateBooksTable(allBooks);
+  }
+};
+
+searchInput.oninput = searchBooks;
 
 var updateBookCount = function(numBooks) {
   bookCountBadge.innerText = numBooks + ' books';
