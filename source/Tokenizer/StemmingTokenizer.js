7d65bacca16c53ae78450c84c7598ff936c3c8e0
Brian Vaughn <bvaughn@fb.com>
2017-02-03 13:33:05 -0800

    Optimized search performance of StemmingTokenizer by removing unnecessary wrapper function

--- source/Tokenizer/StemmingTokenizer.js
+++ source/Tokenizer/StemmingTokenizer.js
@@ -35,9 +35,8 @@ export class StemmingTokenizer implements ITokenizer {
    * @inheritDocs
    */
   tokenize(text : string) : Array<string> {
-    return this._tokenizer.tokenize(text)
-      .map(
-        (token) => this._stemmingFunction(token)
-      );
+    return this._tokenizer
+      .tokenize(text)
+      .map(this._stemmingFunction);
   }
 };
